e480c30bf17dfab39e8245afff81e4bbf6a04fe5
vonkanehoffen <baron.vonkanehoffen@gmail.com>
2024-04-02 17:48:43 +0100

    hacky fix

--- app/(tabs)/things/[thingId]/_layout.tsx
+++ app/(tabs)/things/[thingId]/_layout.tsx
@@ -1,4 +1,4 @@
-import { Stack, Tabs, useLocalSearchParams } from 'expo-router';
+import { Stack, Tabs, router, useLocalSearchParams } from 'expo-router';
 
 export default function ThingsLayout() {
   const { thingId } = useLocalSearchParams<{ thingId: string }>();
@@ -6,9 +6,28 @@ export default function ThingsLayout() {
     <>
       <Stack.Screen options={{ title: `Thing ${thingId}` }} />
       <Tabs>
-        <Tabs.Screen name="index" options={{ title: 'Index' }} />
-        <Tabs.Screen name="status" options={{ title: 'Status' }} />
+        <Tabs.Screen
+          name="index"
+          options={{ title: 'Index' }}
+          listeners={{
+            tabPress: (e) => {
+              e.preventDefault();
+              router.push(`/(tabs)/things/${thingId}/`);
+            },
+          }}
+        />
+        <Tabs.Screen
+          name="status"
+          options={{ title: 'Status' }}
+          listeners={{
+            tabPress: (e) => {
+              e.preventDefault();
+              router.push(`/(tabs)/things/${thingId}/status`);
+            },
+          }}
+        />
       </Tabs>
     </>
   );
 }
+``;
